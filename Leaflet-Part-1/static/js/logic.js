02e024879ec53e097e77d6fa2cecbcdbd7b7e934
kasheshjaiin <jkashish8055@gmail.com>
2024-04-13 12:41:14 -0400

    updating code

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -4,7 +4,7 @@ var myMap = L.map("map", {
     zoom: 2
 });
 
-// Add the base layer
+// Add a tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
